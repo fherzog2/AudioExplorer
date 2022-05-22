2507a0fef83e950d8ee99e1215ea0de77483b7d7
fherzog2 <f.herzog@freenet.de>
2022-05-22 12:42:16 +0200

    Support adding directories with drag&drop. Directories are sorted automatically.

--- src/SettingsEditorWindow.cpp
+++ src/SettingsEditorWindow.cpp
@@ -2,6 +2,9 @@
 #include "SettingsEditorWindow.h"
 
 #include <set>
+#include <QtCore/qcollator.h>
+#include <QtCore/qmimedata.h>
+#include <QtGui/qevent.h>
 #include <QtGui/qstandarditemmodel.h>
 #include <QtWidgets/qboxlayout.h>
 #include <QtWidgets/qmenu.h>
@@ -13,6 +16,88 @@
 #include <QtWidgets/qcombobox.h>
 #include <QtWidgets/qgroupbox.h>
 
+/**
+* Sublcassed to have a more natural sort order.
+*/
+class CustomSortStandardItem : public QStandardItem
+{
+public:
+    CustomSortStandardItem(const QString& text) : QStandardItem(text) {}
+
+    bool operator<(const QStandardItem& other) const override
+    {
+        // sophisticated string comparison
+        QCollator collator;
+        collator.setNumericMode(true);
+        return collator.compare(text(), other.text()) < 0;
+    }
+};
+
+//=============================================================================
+
+/**
+* Subclassed to support drag&drop of directories from the outside.
+*/
+class DirectoryListView : public QListView
+{
+public:
+    DirectoryListView(QWidget* parent, std::function<void(QString)> dir_dropped_callback);
+
+private:
+    void dragEnterEvent(QDragEnterEvent* event) override;
+    void dragMoveEvent(QDragMoveEvent* event) override;
+    void dropEvent(QDropEvent* event) override;
+
+    static std::vector<QString> getPathsFromDropEvent(QDropEvent* event);
+
+    std::function<void(QString)> _dir_dropped_callback;
+};
+
+DirectoryListView::DirectoryListView(QWidget* parent, std::function<void(QString)> dir_dropped_callback)
+    : QListView(parent)
+    , _dir_dropped_callback(dir_dropped_callback)
+{
+    setAcceptDrops(true);
+}
+
+void DirectoryListView::dragEnterEvent(QDragEnterEvent* event)
+{
+    if (!getPathsFromDropEvent(event).empty())
+        event->acceptProposedAction();
+}
+
+void DirectoryListView::dragMoveEvent(QDragMoveEvent* event)
+{
+    // check has happened in dragEnterEvent, no further checks here
+
+    event->acceptProposedAction();
+}
+
+void DirectoryListView::dropEvent(QDropEvent* event)
+{
+    for (const auto& path : getPathsFromDropEvent(event))
+        _dir_dropped_callback(path);
+}
+
+std::vector<QString> DirectoryListView::getPathsFromDropEvent(QDropEvent* event)
+{
+    std::vector<QString> paths;
+
+    if (const QMimeData* mime = event->mimeData())
+    {
+        for (const auto& url : mime->urls())
+        {
+            const QString path = url.toLocalFile();
+            if (!path.isEmpty() && QFileInfo(path).isDir())
+                paths.push_back(path);
+        }
+    }
+
+    return paths;
+}
+
+//=============================================================================
+
 class SettingsWidgetDirPaths : public AbstractSettingsWidget
 {
 public:
@@ -22,6 +107,7 @@ public:
     void applyChanges() const override;
 
 private:
+    void addPath(const QString& path) const;
     void deleteSelectedRows() const;
 
     SettingsItem<QStringList>& _item;
@@ -38,18 +124,18 @@ SettingsWidgetDirPaths::SettingsWidgetDirPaths(QWidget* parent, SettingsItem<QSt
     QPushButton* add_button = new QPushButton(QObject::tr("Add audio directory..."), _container);
     QObject::connect(add_button, &QPushButton::clicked, [this](){
         QString path = QFileDialog::getExistingDirectory();
-        if(!path.isEmpty())
-            _model->setItem(_model->rowCount(), new QStandardItem(path));
+        if (!path.isEmpty())
+            addPath(path);
     });
 
     _model = new QStandardItemModel();
 
     for (const QString& path : item.getValue())
     {
-        _model->setItem(_model->rowCount(), new QStandardItem(path));
+        addPath(path);
     }
 
-    _list = new QListView(_container);
+    _list = new DirectoryListView(_container, [this](QString path) { addPath(path); });
     _list->setModel(_model);
     _list->setContextMenuPolicy(Qt::CustomContextMenu);
     _list->setSelectionMode(QListView::ExtendedSelection);
@@ -91,6 +177,21 @@ void SettingsWidgetDirPaths::applyChanges() const
         paths.push_back(_model->item(i)->text());
 
     _item.setValue(paths);
+}
+
+void SettingsWidgetDirPaths::addPath(const QString& path) const
+{
+    // prevent duplicates
+
+    for (int i = 0, n = _model->rowCount(); i < n; ++i)
+    {
+        const QModelIndex index = _model->index(i, 0);
+        if (index.data().toString() == path)
+            return; // duplicate found
+    }
+
+    _model->setItem(_model->rowCount(), new CustomSortStandardItem(path));
+    _model->sort(0);
 }
 
 void SettingsWidgetDirPaths::deleteSelectedRows() const
